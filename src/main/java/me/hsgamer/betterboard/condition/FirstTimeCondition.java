47d22ef3c88602bde5f75555bdd2d5fc81fbdd0b
Huynh Tien <huynhqtienvtag@gmail.com>
2021-09-22 16:24:02 +0700

    true or false on first time

--- src/main/java/me/hsgamer/betterboard/condition/FirstTimeCondition.java
+++ src/main/java/me/hsgamer/betterboard/condition/FirstTimeCondition.java
@@ -1,11 +1,18 @@
 package me.hsgamer.betterboard.condition;
 
-import me.hsgamer.betterboard.api.condition.Condition;
+import me.hsgamer.betterboard.api.condition.ConfigurableCondition;
 import org.bukkit.entity.Player;
 
-public class FirstTimeCondition implements Condition {
+public class FirstTimeCondition implements ConfigurableCondition {
+    private boolean checkValue = true;
+
     @Override
     public boolean check(Player player) {
-        return !player.hasPlayedBefore();
+        return (!player.hasPlayedBefore()) == checkValue;
+    }
+
+    @Override
+    public void loadFromObject(Object object) {
+        this.checkValue = Boolean.parseBoolean(String.valueOf(object));
     }
 }
